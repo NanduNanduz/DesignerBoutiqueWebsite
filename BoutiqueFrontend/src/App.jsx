e0aec588d772f190c36ab7f33e96f015082492ed
NanduNanduz <nandunsb99@gmail.com>
2025-01-30 02:21:31 +0530

    Updated App.jsx with Main

--- BoutiqueFrontend/src/App.jsx
+++ BoutiqueFrontend/src/App.jsx
@@ -3,16 +3,22 @@ import Navbar from './components/Navbar'
 import { Route, Routes } from "react-router-dom";
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './components/Home';
+import Main from './components/Main';
 
 const App = () => {
   return (
     <div>
       {/* <Home/> */}
-      <Navbar/>
+     
 
       <Routes>
         <Route path="/login" element={<Login />}></Route>
         <Route path='/signup' element={<Signup/>}></Route>
+
+        
+          <Route path='/' element={<Main child={<Home/>}/>}></Route>
+        
       </Routes>
     </div>
   );
