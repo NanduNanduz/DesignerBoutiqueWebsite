9ecc785699c973366b04c3f8f17209fdebf1afc1
NanduNanduz <nandunsb99@gmail.com>
2025-01-30 01:53:28 +0530

    Updated the App.jsx with the signup routes in Routes

--- BoutiqueFrontend/src/App.jsx
+++ BoutiqueFrontend/src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from "react-router-dom";
 import Login from './components/Login';
+import Signup from './components/Signup';
 
 const App = () => {
   return (
     <div>
       {/* <Home/> */}
       <Navbar/>
-      
+
       <Routes>
         <Route path="/login" element={<Login />}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
       </Routes>
     </div>
   );
