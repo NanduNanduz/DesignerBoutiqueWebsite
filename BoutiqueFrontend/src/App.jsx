aac69195541d2836217e10f429d3930d127fc177
NanduNanduz <nandunsb99@gmail.com>
2025-01-29 21:57:50 +0530

    created component Navbar.jsx

--- BoutiqueFrontend/src/App.jsx
+++ BoutiqueFrontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './components/Home'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      {/* <Home/> */}
+       <Navbar/>
       
     </div>
   )
