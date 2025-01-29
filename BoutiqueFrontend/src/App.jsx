d4f0eb50a6d51e74012d0ee4404e8cb72ff9d898
NanduNanduz <nandunsb99@gmail.com>
2025-01-29 23:18:48 +0530

    Updated routes  in the App.jsx  with  /login for login page

--- BoutiqueFrontend/src/App.jsx
+++ BoutiqueFrontend/src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import Home from './components/Home'
 import Navbar from './components/Navbar'
-
+import { Route, Routes } from "react-router-dom";
+import Login from './components/Login';
 
 const App = () => {
   return (
     <div>
       {/* <Home/> */}
-       <Navbar/>
+      <Navbar/>
       
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App
