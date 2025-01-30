d14545e28a5eac64398bda99463495cc0dddecf4
NanduNanduz <nandunsb99@gmail.com>
2025-01-30 15:59:27 +0530

    Created index.js file , in which all the dependencies were required  or imported and created instance  of express, also  used  app.listern

---
+++ BoutiqueBackend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = new express();
+
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+const cors = require("cors");
+app.use(cors());
+
+require("dotenv").config();
+require("./db/connection");
+
+const userRoutes = require("./routes/userRoutes");
+app.use("/users", userRoutes);
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running ${process.env.PORT}`);
+});
