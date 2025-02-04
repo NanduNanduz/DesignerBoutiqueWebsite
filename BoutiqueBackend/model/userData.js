03ebea74653b5d38a78af5b7b07b8f3575ac02d9
NanduNanduz <nandunsb99@gmail.com>
2025-02-04 22:18:36 +0530

    Updated userdate model  with fields

--- BoutiqueBackend/model/userData.js
+++ BoutiqueBackend/model/userData.js
@@ -1,13 +1,27 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = mongoose.Schema({
+//   name: String,
+//   email:String,
+//   phoneNumber:String,
+//   address: String,
+//   password: String,
+//   role: String,
+// });
+
+// const userData = mongoose.model("user", userSchema);
+
+// module.exports = userData;
+
+
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  name: String,
-  email:String,
-  phoneNumber:String,
-  address: String,
-  password: String,
-  role: String,
-});
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  cartData: {type: Object, default: {}}
+}, {minimize:false});
 
 const userData = mongoose.model("user", userSchema);
 
