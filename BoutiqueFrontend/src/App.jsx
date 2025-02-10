278ea3bc52ded002aa2aff053868b1521c9ea224
NanduNanduz <nandunsb99@gmail.com>
2025-02-11 01:13:04 +0530

    Updated App.jsx with privateRoutes

--- BoutiqueFrontend/src/App.jsx
+++ BoutiqueFrontend/src/App.jsx
@@ -1,38 +1,89 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+// import React from 'react'
+// import Navbar from './components/Navbar'
+// import { Route, Routes } from "react-router-dom";
+// import Login from './components/Login';
+// import Signup from './components/Signup';
+// import Home from './components/Home';
+// import Main from './components/Main';
+// import Newcollections from './components/Newcollections';
+// import Bookappointment from './components/Bookappointment';
+// import Kurti from './components/Kurti';
+// import Product from './components/Product';
+// import Lehenga from './components/Lehenga';
+// import Saree from './components/Saree';
+// import AdminDashboard from './components/AdminDashboard'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/login" element={<Login />}></Route>
+//         <Route path="/signup" element={<Signup />}></Route>
+//         <Route path="/new-collections" element={<Newcollections />}></Route>
+//         <Route path="/book-appointment" element={<Bookappointment />}></Route>
+//         <Route path="/kurti" element={<Main child={<Kurti />} />}></Route>
+//         <Route path="/lehenga" element={<Main child={<Lehenga />} />}></Route>
+//         <Route path="/saree" element={<Main child={<Saree />} />}></Route>
+
+//         <Route
+//           path="/product/:productId"
+//           element={<Main child={<Product />} />}></Route>
+
+//         <Route path="/" element={<Main child={<Home />} />}></Route>
+//         <Route path="/admin-dashboard" element={<Main child={<AdminDashboard />} />}></Route>
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App
+
+
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Home from './components/Home';
-import Main from './components/Main';
-import Newcollections from './components/Newcollections';
-import Bookappointment from './components/Bookappointment';
-import Kurti from './components/Kurti';
-import Product from './components/Product';
-import Lehenga from './components/Lehenga';
-import Saree from './components/Saree'
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Main from "./components/Main";
+import Newcollections from "./components/Newcollections";
+import Bookappointment from "./components/Bookappointment";
+import Kurti from "./components/Kurti";
+import Product from "./components/Product";
+import Lehenga from "./components/Lehenga";
+import Saree from "./components/Saree";
+import AdminDashboard from "./components/AdminDashboard";
+import PrivateRoutes from "./components/PrivateRoutes"; // Import PrivateRoutes
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/new-collections" element={<Newcollections />}></Route>
-        <Route path="/book-appointment" element={<Bookappointment />}></Route>
-        <Route path="/kurti" element={<Main child={<Kurti />} />}></Route>
-        <Route path="/lehenga" element={<Main child={<Lehenga />} />}></Route>
-        <Route path="/saree" element={<Main child={<Saree />} />}></Route>
-
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/new-collections" element={<Newcollections />} />
+        <Route path="/book-appointment" element={<Bookappointment />} />
+        <Route path="/kurti" element={<Main child={<Kurti />} />} />
+        <Route path="/lehenga" element={<Main child={<Lehenga />} />} />
+        <Route path="/saree" element={<Main child={<Saree />} />} />
         <Route
           path="/product/:productId"
           element={<Main child={<Product />} />}
-        ></Route>
+        />
+        <Route path="/" element={<Main child={<Home />} />} />
 
-        <Route path="/" element={<Main child={<Home />} />}></Route>
+        {/*  Protected Admin Route */}
+        <Route element={<PrivateRoutes requiredRole="admin" />}>
+          <Route
+            path="/admin-dashboard"
+            element={<Main child={<AdminDashboard />} />}
+          />
+        </Route>
       </Routes>
     </div>
   );
-}
+};
 
-export default App
+export default App;
+
--- BoutiqueFrontend/src/components/PrivateRoutes.jsx
+++ BoutiqueFrontend/src/components/PrivateRoutes.jsx
@@ -1,17 +1,41 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import { Navigate } from "react-router-dom";
+// import React from "react";
+// import { Outlet } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 
-const PrivateRoutes = () => {
+// const PrivateRoutes = () => {
+//   const token = sessionStorage.getItem("logintoken");
+//   let verifyUser = false;
+//   if (token) {
+//     verifyUser = true;
+//   }
+
+//   return verifyUser ? <Outlet /> : <Navigate to={"/"} />;
+// };
+
+// /* outlet is a respective component .it will load when the user is verified */
+
+// export default PrivateRoutes;
+
+
+
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
+
+const PrivateRoutes = ({ requiredRole }) => {
   const token = sessionStorage.getItem("logintoken");
-  let verifyUser = false;
-  if (token) {
-    verifyUser = true;
+  const role = sessionStorage.getItem("role");
+
+  if (!token) {
+    return <Navigate to="/login" />;
   }
 
-  return verifyUser ? <Outlet /> : <Navigate to={"/"} />;
+  // If a required role is provided, check if the user matches it
+  if (requiredRole && role !== requiredRole) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
 };
 
-/* outlet is a respective component .it will load when the user is verified */
-
 export default PrivateRoutes;
+
