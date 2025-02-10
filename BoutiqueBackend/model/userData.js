2fc0b1fd0e904d7968d91bcf3297e6c98898eae9
NanduNanduz <nandunsb99@gmail.com>
2025-02-11 00:42:38 +0530

    Updated userData with role

--- BoutiqueBackend/model/userData.js
+++ BoutiqueBackend/model/userData.js
@@ -20,6 +20,7 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: {type: String},
   cartData: {type: Object, default: {}}
 }, {minimize:false});
 
