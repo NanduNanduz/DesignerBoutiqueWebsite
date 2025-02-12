89dcaa2587233e150d8c340a1c6546b59e3ef6fa
NanduNanduz <nandunsb99@gmail.com>
2025-02-13 01:47:22 +0530

    Updated usermodel to store cart item as an array

--- BoutiqueBackend/model/userData.js
+++ BoutiqueBackend/model/userData.js
@@ -1,14 +1,42 @@
 
+// const mongoose = require("mongoose");
+
+// const userSchema = mongoose.Schema({
+//   name: { type: String, required: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   role: {type: String},
+//   cartData: {type: Object, default: {}}
+// }, {minimize:false});
+
+// const userData = mongoose.model("user", userSchema);
+
+// module.exports = userData;
+
+
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: {type: String},
-  cartData: {type: Object, default: {}}
-}, {minimize:false});
+const cartItemSchema = new mongoose.Schema({
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+  name: String,
+  price: Number,
+  size: String,
+  image: String,
+  quantity: { type: Number, default: 1 },
+});
 
-const userData = mongoose.model("user", userSchema);
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String },
+    cartData: {
+      items: [cartItemSchema], // Store cart items as an array
+    },
+  },
+  { minimize: false }
+);
 
-module.exports = userData;
+const User = mongoose.model("User", userSchema);
+module.exports = User;
