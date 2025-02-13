5eea5aa68433305f02fff931f11aeb9e62b91d10
NanduNanduz <nandunsb99@gmail.com>
2025-02-13 12:45:59 +0530

    Updated navbar which not show all other features except logout in the admin panel

--- BoutiqueFrontend/src/components/Navbar.jsx
+++ BoutiqueFrontend/src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 
-
-
 // import { Link } from "react-router-dom";
 // import { useState, useEffect } from "react";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
 // import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+// import axios from "axios";
 
 // const Navbar = () => {
 //   const [userRole, setUserRole] = useState(null);
@@ -17,21 +16,21 @@
 //       setUserRole(role);
 //     }
 
-//     // Function to update cart count dynamically
-//     const updateCartCount = () => {
-//       const cart = JSON.parse(localStorage.getItem("cart")) || [];
-//       setCartCount(cart.length);
-//     };
-
-//     updateCartCount(); // Initial count update
-
-//     // Listen for cart changes
-//     window.addEventListener("storage", updateCartCount);
-
-//     return () => {
-//       window.removeEventListener("storage", updateCartCount);
-//     };
+//     loadCartCount();
 //   }, []);
+
+//   const loadCartCount = async () => {
+//     try {
+//       const token = sessionStorage.getItem("logintoken");
+//       const response = await axios.get("http://localhost:3000/cart/cartlist", {
+//         headers: { Authorization: `Bearer ${token}` },
+//       });
+
+//       setCartCount(response.data.length);
+//     } catch (error) {
+//       console.error("Error fetching cart count:", error);
+//     }
+//   };
 
 //   const handleLogout = () => {
 //     sessionStorage.clear();
@@ -162,8 +161,6 @@
 
 
 
-
-
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -187,6 +184,8 @@ const Navbar = () => {
   const loadCartCount = async () => {
     try {
       const token = sessionStorage.getItem("logintoken");
+      if (!token) return;
+
       const response = await axios.get("http://localhost:3000/cart/cartlist", {
         headers: { Authorization: `Bearer ${token}` },
       });
@@ -209,6 +208,7 @@ const Navbar = () => {
       style={{ backgroundColor: "#EED9C4", color: "white" }}
     >
       <div className="container-fluid">
+        {/* Logo */}
         <a className="navbar-brand" href="/">
           <svg width="250" height="70" viewBox="0 0 250 70">
             <text
@@ -230,92 +230,105 @@ const Navbar = () => {
           </svg>
         </a>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+        {/* Admin View: Only show logo & logout */}
+        {userRole === "admin" ? (
+          <div className="ms-auto">
+            <Link to="/" className="text-dark" onClick={handleLogout}>
+              Logout
+            </Link>
+          </div>
+        ) : (
+          <>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mx-auto gap-4">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                data-bs-toggle="dropdown"
-              >
-                Shop
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" to="/kurti">
-                    Kurti
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav mx-auto gap-4">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
                   </Link>
                 </li>
-                <li>
-                  <Link className="dropdown-item" to="/lehenga">
-                    Lehenga
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    data-bs-toggle="dropdown"
+                  >
+                    Shop
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <Link className="dropdown-item" to="/kurti">
+                        Kurti
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/lehenga">
+                        Lehenga
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" to="/saree">
+                        Saree
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/new-collections">
+                    New Collections
                   </Link>
                 </li>
-                <li>
-                  <Link className="dropdown-item" to="/saree">
-                    Saree
+                <li className="nav-item">
+                  <Link className="nav-link" to="/book-appointment">
+                    Customized
                   </Link>
                 </li>
               </ul>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/new-collections">
-                New Collections
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/book-appointment">
-                Customized
-              </Link>
-            </li>
-          </ul>
 
-          <div className="d-flex align-items-center gap-3">
-            {userRole === "user" && (
-              <>
-                <Link to="/profile" className="text-dark">
-                  <FontAwesomeIcon icon={faUser} size="lg" />
-                </Link>
-                <Link to="/wishlist" className="text-dark position-relative">
-                  <FontAwesomeIcon icon={faHeart} size="lg" />
-                  <span className="ms-1">(0)</span>
-                </Link>
-                <Link to="/cart" className="text-dark">
-                  <ShoppingCartOutlinedIcon
-                    style={{ fontSize: "1.5rem", color: "#333" }}
-                  />
-                  <span>({cartCount})</span>
-                </Link>
-              </>
-            )}
+              {/* Right side icons */}
+              <div className="d-flex align-items-center gap-3">
+                {userRole === "user" && (
+                  <>
+                    <Link to="/profile" className="text-dark">
+                      <FontAwesomeIcon icon={faUser} size="lg" />
+                    </Link>
+                    <Link
+                      to="/wishlist"
+                      className="text-dark position-relative"
+                    >
+                      <FontAwesomeIcon icon={faHeart} size="lg" />
+                      <span className="ms-1">(0)</span>
+                    </Link>
+                    <Link to="/cart" className="text-dark">
+                      <ShoppingCartOutlinedIcon
+                        style={{ fontSize: "1.5rem", color: "#333" }}
+                      />
+                      <span>({cartCount})</span>
+                    </Link>
+                  </>
+                )}
 
-            {userRole && (
-              <Link to="/" className="text-dark" onClick={handleLogout}>
-                Logout
-              </Link>
-            )}
-
-            {!userRole && (
-              <Link to="/login" className="text-dark">
-                Login
-              </Link>
-            )}
-          </div>
-        </div>
+                {userRole ? (
+                  <Link to="/" className="text-dark" onClick={handleLogout}>
+                    Logout
+                  </Link>
+                ) : (
+                  <Link to="/login" className="text-dark">
+                    Login
+                  </Link>
+                )}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </nav>
   );
