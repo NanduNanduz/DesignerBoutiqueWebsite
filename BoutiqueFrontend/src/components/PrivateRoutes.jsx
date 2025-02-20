2a16aade2b96f7843ed11a51cfb6f81713a23252
NanduNanduz <nandunsb99@gmail.com>
2025-02-20 22:59:51 +0530

    Update PrivateRoutes.jsx

--- BoutiqueFrontend/src/components/PrivateRoutes.jsx
+++ BoutiqueFrontend/src/components/PrivateRoutes.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-// import { Outlet, Navigate } from "react-router-dom";
-
-// const PrivateRoutes = ({ requiredRole, children }) => {
-//   const token = sessionStorage.getItem("logintoken");
-//   const role = sessionStorage.getItem("role");
-
-//   if (!token) {
-//     return <Navigate to="/login" />;
-//   }
-
-//   if (requiredRole && role !== requiredRole) {
-//     return <Navigate to="/" />;
-//   }
-
-//   return children;
-// };
-
-// export default PrivateRoutes;
-
 import React from "react";
 import { Outlet, Navigate, useLocation } from "react-router-dom";
 
